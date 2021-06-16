06e40f1eaf9f2584978280377be687142427cf9f
kreynoldsf5 <k.reynolds@f5.com>
2021-06-16 10:05:17 -0400

    reworked loadgen for user-agent

--- loadgen-container/crawler.js
+++ loadgen-container/crawler.js
@@ -1,18 +1,7 @@
 import http from 'k6/http';
 import { sleep, check } from 'k6';
 import {parseHTML} from "k6/html";
-
-export function addRandAgent() {
-    var agentHeaders = [
-        {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'},
-        {'User-Agent': 'Mozilla/5.0 CK={} (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko'},
-        {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Brave Chrome/83.0.4103.116 Safari/537.36'}
-    ]
-    var agentHeader = {
-        headers: agentHeaders[Math.floor(Math.random()*agentHeaders.length)]
-    }
-    return agentHeader;
-}
+import { addRandAgent } from './helpers.js';
 
 export function crawler() {
     const base = `${__ENV.TARGET_URL}`
