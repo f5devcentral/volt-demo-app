bb1c8f6ee4eddbbcfcbbb7830a38f0e545aa24b9
kreynoldsf5 <k.reynolds@f5.com>
2022-02-07 21:24:35 -0500

    expanded custom_error to all 500s

--- terraform/modules/volterra/main.tf
+++ terraform/modules/volterra/main.tf
@@ -323,7 +323,7 @@ resource "volterra_http_loadbalancer" "proxy" {
   more_option {
     custom_errors = {
       408 = format("string:///%s", filebase64("${path.module}/error-page.html"))
-      503 = format("string:///%s", filebase64("${path.module}/error-page.html"))
+      5 = format("string:///%s", filebase64("${path.module}/error-page.html"))
     }
   }
   disable_rate_limit              = true
